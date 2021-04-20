de81c6e6117135195bf93ce3c7d271eb14db79f1
Li Qiang <1273553780@qq.com>
2021-04-20 21:34:42 +0800

    first

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,9 +15,9 @@ export default {
         // subFolders: false,
         dir: "docs",
     },
-    router: {
-        base: "./",
-    },
+    // router: {
+    //     base: "./",
+    // },
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
         title: "web-recorder",
@@ -29,7 +29,7 @@ export default {
             { name: "viewport", content: "width=device-width, initial-scale=1" },
             { hid: "description", name: "description", content: "" },
         ],
-        link: [{ rel: "icon", type: "image/x-icon", href: "./favicon.ico" }],
+        // link: [{ rel: "icon", type: "image/x-icon", href: "./favicon.ico" }],
     },
 
     // Global CSS: https://go.nuxtjs.dev/config-css

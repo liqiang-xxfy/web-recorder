a28a1eb3366d1c3f2b458ce462b4dce181b3f8eb
Li Qiang <1273553780@qq.com>
2021-04-20 21:32:20 +0800

    first

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ export default {
             { name: "viewport", content: "width=device-width, initial-scale=1" },
             { hid: "description", name: "description", content: "" },
         ],
-        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+        link: [{ rel: "icon", type: "image/x-icon", href: "./favicon.ico" }],
     },
 
     // Global CSS: https://go.nuxtjs.dev/config-css
